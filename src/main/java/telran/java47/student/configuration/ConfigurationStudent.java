c35f699e2df50cd50cdb9e5c7080a04d06903dbc
admin <admin@AVLivshits-h.mshome.net>
2023-06-08 18:57:13 +0300

    first commit

---
+++ src/main/java/telran/java47/student/configuration/ConfigurationStudent.java
@@ -0,0 +1,15 @@
+package telran.java47.student.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationStudent {
+	@Bean
+	 public ModelMapper getModelMapper() {
+		return null;
+		 
+	 }
+
+}
